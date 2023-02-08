78f53301145af0568760e3ad2fe64747ce7ac29c
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-02-08 20:34:06 +0100

    small fixes to gui

--- java-stuff.js
+++ java-stuff.js
@@ -1,2 +1,3 @@
 export const Executors = Java.type("java.util.concurrent.Executors")
 export const File = Java.type("java.io.File")
+export const ResourceLocation = Java.type("net.minecraft.util.ResourceLocation")
