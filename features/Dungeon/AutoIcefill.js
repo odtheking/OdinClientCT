392b0376dbedfe339b0f4939faeb81795e24587b
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-04-03 18:32:07 +0200

    start on ice fill

---
+++ features/Dungeon/AutoIcefill.js
@@ -0,0 +1,18 @@
+
+
+function checkRotation() {
+    const px = Math.floor(Player.getX()); const py = Math.floor(Player.getY()); const pz = Math.floor(Player.getZ())  
+    if (getBlockNameAt(px+4,py,pz) == "minecraft:stone_brick_stairs") {
+        rotation = "east"
+    } else if (getBlockNameAt(px-4,py,pz) == "minecraft:stone_brick_stairs") {
+        rotation = "west"
+    } else if (getBlockNameAt(px,py,pz+4) == "minecraft:stone_brick_stairs") {
+        rotation = "south"
+    } else if (getBlockNameAt(px,py,pz-4) == "minecraft:stone_brick_stairs") {
+        rotation = "north"
+    }
+}
+
+const getBlockNameAt = (x,y,z) => {
+    return World.getBlockAt(new BlockPos(x,y,z)).type.name
+}
